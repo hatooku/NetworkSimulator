1f122b971aaa92ba867e6997e29282248f071146
Malavika Venkatesan <malavika1996@hotmail.com>
2016-11-09 17:31:01 -0800

    made a plan for routers in pseudocode, made links a dict instead of an array

--- router.py
+++ router.py
@@ -6,14 +6,14 @@ class Router(Node):
     Attributes:
         ns (NetworkSimulator): An instance of the NetworkSimulator class.
         node_id (string): The network address of the router.
-        links (Link []): The links that the router is connected to.
+        links (Link dict): The links that the router is connected to.
         routing_table (dict): The routing table for the router.
 
     """
 
     def __init__(self, ns, node_id):
         Node.__init__(self, ns, node_id)
-        self.links = []
+        self.links = {}
         self.routing_table = {}
 
     def add_link(self, link):
@@ -23,7 +23,7 @@ class Router(Node):
             link (Link): The link to add.
 
         """
-        self.links.append(link)
+        self.links[link.link_id] = link
 
     def send_packet(self, packet):
         """Sends a packet to another node.
@@ -32,6 +32,8 @@ class Router(Node):
             packet (Packet): The packet to send.
 
         """
+
+        # Use the routing table to send a packet to the right node
         pass
 
     def receive_packet(self, packet):
@@ -44,8 +46,22 @@ class Router(Node):
             packet (Packet): The packet we received.
 
         """
+
+        # if we get a routing packet:
+        #   update_routing_table(packet)
+        # else: (we need to send a data packet or ack packet to its dest host)
+        #   call send_packet(packet)
         pass
 
-    def make_routing_table(self):
-        """Makes the routing table based on the links."""
+    def update_routing_table(self, routing_packet):
+        """Updates the routing table based on the routing packet the router just
+        received.
+
+        Args:
+            routing_packet (Packet): The routing packet we received.
+
+        """
+
+        # Check if we should update the routing table
+        # If we do update the routing table, send out routing packets
         pass
