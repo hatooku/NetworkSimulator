4b30bca0f01c77eb4eb206bbf9d0dd51ee418dce
hatooku <jhlee@caltech.edu>
2016-11-29 06:43:12 -0800

    Redesign link

--- link.py
+++ link.py
@@ -1,4 +1,5 @@
 from collections import deque
+from packet import *
 
 class Link(object):
     """A link which connects new nodes in the graph.
@@ -28,18 +29,16 @@ class Link(object):
         self._max_buffer_size = max_buffer_size
         self._cur_buffer_size = 0.0
   
-
         self._prop_delay = prop_delay
         self._capacity = capacity
-        self._is_transmitting = False
         
         if len(nodes) != 2:
             raise AttributeError("Node array should contain two nodes")
         self.nodes = nodes
         
         self.link_buffer = deque()
-        self._packets_in_route = 0
-        self._cur_destination = None
+        self._packets_in_route = deque()
+        self.counter = 0
         
         
     @property
@@ -89,23 +88,18 @@ class Link(object):
     @packets_in_route.setter
     def packets_in_route(self, value):
         raise AttributeError("Current packet should not be changed externally")
-    
-    @property
-    def cur_destination(self):
-        return self._cur_destination
-    
-    @cur_destination.setter
-    def cur_destination(self, value):
-        raise AttributeError("Current destination cannot be changed externally") 
 
-    @property
-    def is_transmitting(self):
-        return self._is_transmitting
-    
-    @is_transmitting.setter
-    def is_transmitting(self, value):
-        raise AttributeError("Transmitting status cannot be changed externally")    
-   
+    def _get_cur_destination(self):
+        if len(self._packets_in_route) == 0:
+            return None
+
+        first_packet_info = self._packets_in_route[0]
+        dest = first_packet_info[1]
+
+        for packet_info in self._packets_in_route:
+            assert dest.node_id == packet_info[1].node_id
+
+        return dest
 
     def _get_other_node(self, node_id):
         """Finds the node that node with id node_id is linked to.
@@ -125,7 +119,7 @@ class Link(object):
             destination = self.nodes[0]   
         else:
             raise Exception("This link is not connected to node with"
-                            "node_id %s" % node_id)    
+                            "node_id %s" % node_id)
         return destination        
         
     def get_other_node_id(self, node_id):
@@ -153,19 +147,23 @@ class Link(object):
             packet (Packet): the packet being sent
             node_id (string): the id of the node sending the packet 
             
-        """  
+        """
+
         if self.cur_buffer_size + packet.packet_size <= self.max_buffer_size:
             
             destination = self._get_other_node(node_id)
+            self.counter +=1
             self.link_buffer.append((packet, destination))
             self._cur_buffer_size += packet.packet_size
+
+            # Assert _cur_buffer_size is correct
+            assert self._cur_buffer_size == sum([packet_info[0].packet_size for packet_info in self.link_buffer])
             
             self.ns.record_buffer_occupancy(self.link_id, len(self.link_buffer))
 
-            if len(self.link_buffer) == 1 and not self.is_transmitting:
-                if self.packets_in_route == 0 or \
-                   destination == self.cur_destination:
-
+            if len(self.link_buffer) == 1:
+                cur_destination = self._get_cur_destination()
+                if cur_destination is None or destination == cur_destination or len(self.packets_in_route) == 0:
                     self.start_packet_transmission()
         else:
             print "Link %s is full; packet %s is dropped @ t=%f" \
@@ -181,23 +179,17 @@ class Link(object):
             
         """
 
-        assert not self.is_transmitting
-        self._is_transmitting = True
+        assert len(self.link_buffer) > 0
 
-        packet_info = self.link_buffer[0]
-
-        assert self._cur_destination is None \
-                or self._cur_destination == packet_info[1]
-
-        packet_size = packet_info[0].packet_size
+        packet, destination = self.link_buffer[0]
+        cur_destination = self._get_cur_destination()
+        assert cur_destination is None or cur_destination == destination
 
         event = lambda: self.start_packet_propagation()
-        trans_delay = 1.0 * packet_size / self.capacity
+        trans_delay = 1.0 * packet.packet_size / self.capacity
         
         self.ns.add_event(event, "Link.start_packet_propagation() with"
                               " link_id = %s" % (self.link_id), trans_delay)
-        
-        self._cur_destination = packet_info[1]
 
 
     def start_packet_propagation(self):
@@ -208,28 +200,28 @@ class Link(object):
         Calls finish_packet_transfer after the propagation delay.
         
         """
-        packet_info = self.link_buffer.popleft()
-        self._packets_in_route += 1
-        packet_size = packet_info[0].packet_size
-        self._cur_buffer_size -= packet_size
-
+        packet, destination = self.link_buffer.popleft()
+        self._cur_buffer_size -= packet.packet_size
         self.ns.record_buffer_occupancy(self.link_id, len(self.link_buffer))
-       
-        self._is_transmitting = False
-       
-        event = lambda: self.finish_packet_transfer(packet_info[0])
-        self.ns.add_event(event, "Link.finish_packet_transfer() with"
-                          " link_id = %s" % self.link_id, self.prop_delay)
 
-        assert self.cur_destination is not None
+        self._packets_in_route.append((packet, destination))
+        assert self._get_cur_destination() == destination
+       
+        if isinstance(packet, AcknowledgementPacket):
+            event = lambda: self.finish_packet_transfer()
+            self.ns.add_event(event, "Link.finish_packet_transfer() with"
+                              " link_id = %s" % self.link_id)
+        else:
+            event = lambda: self.finish_packet_transfer()
+            self.ns.add_event(event, "Link.finish_packet_transfer() with"
+                              " link_id = %s" % self.link_id, self.prop_delay)
 
         if len(self.link_buffer) > 0:
-            next_packet_info = self.link_buffer[0]
-            next_destination = next_packet_info[1]
-            if next_destination == self.cur_destination:
+            next_destination = self.link_buffer[0][1]
+            if next_destination == self._get_cur_destination():
                 self.start_packet_transmission()
    
-    def finish_packet_transfer(self, packet):
+    def finish_packet_transfer(self):
         """Hand off the packet to the node it was going to. 
         
         When this method is called, the packet will be transferred to the 
@@ -238,28 +230,24 @@ class Link(object):
         packet will be sent.
         
         """
-        assert self.packets_in_route > 0
-        assert self.cur_destination != None
-        cur_destination = self.cur_destination
-        self._packets_in_route -= 1
-        event = lambda: cur_destination.receive_packet(packet, self.link_id)
+        assert len(self.packets_in_route) > 0
+        cur_destination = self._get_cur_destination()
+        packet, destination = self._packets_in_route.popleft()
+        assert destination == cur_destination
+
+        event = lambda: destination.receive_packet(packet, self.link_id)
         self.ns.add_event(event, "Node.receive_packet() with node_id = %s, "
                           "cur_packet = %s, link_id = %s" \
                           % (cur_destination.node_id, packet.packet_id, 
                              self.link_id))
         
         self.ns.record_link_rate(self.link_id, packet.packet_size)
-        
-        # If there are no links being transmitted or propagated,
-        # set current destination to None.
-        if not self.is_transmitting and self.packets_in_route == 0:
-            self._cur_destination = None
 
         # Start transmitting next packet buffer if applicable.
-        if len(self.link_buffer) > 0 and not self.is_transmitting:
-            #next_packet_info = self.link_buffer[0]
-            #next_destination = next_packet_info[1]
-
-            if self.cur_destination is None:
+        if len(self.link_buffer) > 0 and len(self._packets_in_route) == 0:
+            next_packet, next_destination = self.link_buffer[0]
+            # If destination == next_destination, it is already transmitting
+            if destination != next_destination:
+                assert self._get_other_node(destination.node_id) == next_destination
                 self.start_packet_transmission()
     
